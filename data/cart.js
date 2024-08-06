c0919cbf8e94b07d32b71fd2386d2b8ccbbe2b73
lokesh <gembali.lokesh2004@gmail.com>
2024-08-06 22:48:48 +0530

    delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart=[
+export let cart=[
   {
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity:2
@@ -27,7 +27,7 @@ export function addtoCart(productId){
   
   if(matchingItem){
   
-    matchingItem.quantity+=quantity;
+    matchingItem.quantity+=quantity; 
   
   }
   
@@ -37,4 +37,13 @@ export function addtoCart(productId){
   
   }
   
+}
+export function removeFromCart(productId){
+  const newCart=[];
+  cart.forEach((cartItem)=>{
+    if(cartItem.productId!=productId){
+      newCart.push(cartItem);
+    }
+  });
+cart=newCart;
 }
