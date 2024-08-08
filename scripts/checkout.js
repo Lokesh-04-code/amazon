95c05b7be9826b81dc41482e2a1c64fcc488d4b6
lokesh <gembali.lokesh2004@gmail.com>
2024-08-08 21:17:04 +0530

    delete products from with clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,10 @@
 import {cart,removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
-let cartSummaryHTML='';
+
+
+
+  let cartSummaryHTML='';
 cart.forEach((cartItem)=>{
   const productId=cartItem.productId;
   let matchingProduct;
@@ -12,7 +15,7 @@ if(productId===product.id){
   });
  cartSummaryHTML+= 
 
-  `  <div class="cart-item-container">
+  `  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
             <div class="cart-item-details-grid">
@@ -86,6 +89,7 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
   link.addEventListener('click',()=>{
      const productId=link.dataset.productId;
      removeFromCart(productId);
-     console.log(cart);
+   const container =document.querySelector(`.js-cart-item-container-${productId}`);
+     container.remove();
   });
 });
